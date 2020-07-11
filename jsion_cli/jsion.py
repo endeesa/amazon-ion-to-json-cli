41ab90c16b365914e4d3c55f34260833f3d448de
Banzyme <nbanzyme@gmail.com>
2020-07-11 21:21:14 +0000

    update api

--- jsion_cli/jsion.py
+++ jsion_cli/jsion.py
@@ -1,6 +1,9 @@
+import json
+import amazon.ion.simpleion as ion
 from abc import ABC, abstractmethod, abstractproperty
 from cmdline_options_reader import read_cmdline_options, CommandLineOptions
-from shared_module import JSIONException
+from shared_module import JSIONException, read_file
+from pyion2json import ion_to_json
 
 
 class ConverterConfiguration:
@@ -15,8 +18,8 @@ class ConverterConfiguration:
 
 
 class JSIONConverterBase(ABC):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, config) -> None:
+        self._config = config
 
     @abstractmethod
     def convert():
@@ -24,12 +27,27 @@ class JSIONConverterBase(ABC):
 
 
 class JSIONConvertIonToJson(JSIONConverterBase):
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def __load_actions(self):
+        pass
+
+    
+    def convert():
+        ion_data_str = u'%s' % read_file()
+        ion_data = ion.loads(ion_data_str, single_value=False)
+        #pretty_ion_data = ion.dumps(ion_data, binary=False, indent='  ')
+        json_doc = ion_to_json(ion_data[0])
+        json_data = json.dumps(json_doc, indent=' ')
+        return json_data
+
 
 
 
 class JSIONConvertJsonToIon(JSIONConverterBase):
-    pass
+    def convert():
+        print('ION to Json conversion not yet supported')
 
 
 
@@ -40,7 +58,7 @@ def converter_factory(cmdline_options: CommandLineOptions) -> JSIONConverterBase
 
 
 def main():
-    print("JSION cli converter init...")
+    print("JsIon cli converter init...")
     cmdline_options = read_cmdline_options()
     print(cmdline_options)
 
