d85d0adac7efb05b91cb326d03ee3868c202b5df
Banzyme <nbanzyme@gmail.com>
2020-07-11 17:11:37 +0000

    add shared mmodule

--- jsion_cli/jsion.py
+++ jsion_cli/jsion.py
@@ -1,1 +1,10 @@
-from cli_args_reader import read_cmdline_options
+from cmdline_options_reader import read_cmdline_options
+from shared_module import JSIONException
+
+
+def main():
+    cmdline_options = read_cmdline_options()
+    print(cmdline_options)
+
+if __name__ == "__main__":
+    main()
---
+++ jsion_cli/shared_module.py
@@ -0,0 +1,2 @@
+class JSIONException(Exception):
+    pass
