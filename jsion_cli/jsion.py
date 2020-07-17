c9bcfd73310de827d2a96e6f29cf11c5cad6f39d
Banzyme <nbanzyme@gmail.com>
2020-07-17 11:49:22 +0000

    Update converter

--- jsion_cli/jsion.py
+++ jsion_cli/jsion.py
@@ -18,7 +18,7 @@ class ConverterConfiguration:
 
 
 class JSIONConverterBase(ABC):
-    def __init__(self, config) -> None:
+    def __init__(self, config: CommandLineOptions) -> None:
         self._config = config
 
     @abstractmethod
@@ -28,10 +28,39 @@ class JSIONConverterBase(ABC):
 
 class JSIONConvertIonToJson(JSIONConverterBase):
     def __init__(self) -> None:
-        pass
+        self._input_data = None
+        self._output_data = None
+        self._ion_data = None
+
+    def __load_input_data_from_file(self) -> str:
+        input_data = u'%s' % read_file(filename=self._config.input_file)
+        if input_data in (None, ''):
+            raise JSIONException('Invalid input: \n %s' % input_data)
+        self._input_data = input_data
+        return input_data
+
+    def __load_ion_data(self):
+        ion_data = ion.loads(ion_data_str, single_value=False)
+        if isinstance(ion_data, list):
+            self._ion_data = ion_data[0]   # TODO: what if len > 1?
+        self._ion_data = ion_data
+        return ion_data
+    
+    def __convert_ion_to_json(self):
+        json_object = ion_to_json(self._ion_data)
+        json_string = json.dumps(json_object)
+        self._output_data = json_string
+        return json_string
+
 
     def __load_actions(self):
-        pass
+        """
+        Read input file
+        Load ion file
+        convert to output
+        prettify output
+        """
+        
 
     
     def convert():
